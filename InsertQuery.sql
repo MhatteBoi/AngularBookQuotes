dd0c6950a1c7ebfd366f4f5abaacf1fa0260d842
Mhattias Roswall <128820684+MhatteBoi@users.noreply.github.com>
2024-09-13 15:08:34 +0200

    Delete InsertQuery.sql

--- InsertQuery.sql
+++
@@ -1,8 +0,0 @@
-﻿--gettinge all columns
-SELECT * FROM HumanResources.Employee
-
---filter
-SELECT * From HumanResources.Employee ORDER BY SickLeaveHours
-
---sort
-SELECT Employee, SickLeaveHours FROM HumanResources.Employee Order BY SickLeaveHours; 
